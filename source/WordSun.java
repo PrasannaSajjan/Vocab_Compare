f58490f19fd3fbe2ad594aab27d2c2314f701108
Prasanna Sajjan <prasanna.sajjan@gmail.com>
2016-04-03 21:09:37 -0400

    Vocab Comparison of Macbeth & Romeo and Juliet

---
+++ source/WordSun.java
@@ -0,0 +1,282 @@
+import java.io.BufferedReader;
+import java.lang.*;
+import java.io.*;
+import java.net.URI;
+import java.util.HashSet;
+import java.util.Set;
+import java.io.IOException;
+import java.util.regex.Pattern;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.util.StringUtils;
+import org.apache.log4j.Logger;
+
+public class WordSun extends Configured implements Tool {
+
+  private static final Logger LOG = Logger.getLogger(WordSun.class);
+
+  public static void main(String[] args) throws Exception {
+    int res = ToolRunner.run(new WordSun(), args);
+    System.exit(res);
+  }
+
+  public int run(String[] args) throws Exception {
+    Job job = Job.getInstance(getConf(), "wordsun");
+    for (int i = 0; i < args.length; i += 1) {
+      if ("-skip".equals(args[i])) {
+        job.getConfiguration().setBoolean("wordsun.skip.patterns", true);
+        i += 1;
+        job.addCacheFile(new Path(args[i]).toUri());
+        // this demonstrates logging
+        LOG.info("Added file to the distributed cache: " + args[i]);
+      }
+    }
+    job.setJarByClass(this.getClass());
+ // Use TextInputFormat, the default unless job.setInputFormatClass is used
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    job.setMapperClass(Map.class);
+    job.setCombinerClass(Reduce.class);
+    job.setReducerClass(Reduce.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    return job.waitForCompletion(true) ? 0 : 1;
+  }
+
+  public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+    private boolean caseSensitive = false;
+    private long numRecords = 0;
+    private String input;
+    private Set<String> patternsToSkip = new HashSet<String>();
+    private static final Pattern WORD_BOUNDARY = Pattern.compile("\\s*\\b\\s*");
+
+    protected void setup(Mapper.Context context)
+        throws IOException,
+        InterruptedException {
+      if (context.getInputSplit() instanceof FileSplit) {
+        this.input = ((FileSplit) context.getInputSplit()).getPath().toString();
+      } else {
+        this.input = context.getInputSplit().toString();
+      }
+      Configuration config = context.getConfiguration();
+      this.caseSensitive = config.getBoolean("wordsun.case.sensitive", false);
+      if (config.getBoolean("wordsun.skip.patterns", false)) {
+        URI[] localPaths = context.getCacheFiles();
+        parseSkipFile(localPaths[0]);
+      }
+    }
+
+    private void parseSkipFile(URI patternsURI) {
+      LOG.info("Added file to the distributed cache: " + patternsURI);
+      try {
+        BufferedReader fis = new BufferedReader(new FileReader(new File(patternsURI.getPath()).getName()));
+        String pattern;
+        while ((pattern = fis.readLine()) != null) {
+          patternsToSkip.add(pattern);
+        }
+      } catch (IOException ioe) {
+  System.err.println("Caught exception while parsing the cached file '"
+            + patternsURI + "' : " + StringUtils.stringifyException(ioe));
+      }
+    }
+
+    public void map(LongWritable offset, Text lineText, Context context)
+        throws IOException, InterruptedException {
+      String line = lineText.toString();
+      if (!caseSensitive) {
+        line = line.toLowerCase();
+      }
+      Text currentWord = new Text();
+	  Stemmer s = new Stemmer();
+      for (String word : WORD_BOUNDARY.split(line)) {
+        if (word.isEmpty() || patternsToSkip.contains(word) || Character.isDigit(word.charAt(0))) {
+            continue;
+        }
+             for (int c=0; c<word.length();c++) 
+                 s.add(word.charAt(c));
+                 s.stem();
+
+                 String pattern;
+                 pattern = s.toString();
+                pattern=pattern.replaceAll("[^a-zA-Z0-9]", "");
+                if(pattern.equals(""))
+                continue;
+            currentWord = new Text(pattern);
+	    context.write(currentWord,one);
+        }
+    }
+  }
+
+public static class Stemmer
+{  
+   private char[] b;
+   private int i,     /* offset into b */
+               i_end, /* offset to end of stemmed word */
+               j, k;
+   private static final int INC = 50;
+                     /* unit of size whereby b is increased */
+   public Stemmer()
+   {  
+	  b = new char[INC];
+      i = 0;
+      i_end = 0;
+   }
+
+   public void add(char ch)
+   {  if (i == b.length)
+      {  char[] new_b = new char[i+INC];
+         for (int c = 0; c < i; c++) new_b[c] = b[c];
+         b = new_b;
+      }
+      b[i++] = ch;
+   }
+
+
+
+   public void add(char[] w, int wLen)
+   {  if (i+wLen >= b.length)
+      {  char[] new_b = new char[i+wLen+INC];
+         for (int c = 0; c < i; c++) new_b[c] = b[c];
+         b = new_b;
+      }
+      for (int c = 0; c < wLen; c++) b[i++] = w[c];
+   }
+
+   public String toString() { return new String(b,0,i_end); }
+
+   public int getResultLength() { return i_end; }
+
+   public char[] getResultBuffer() { return b; }
+
+
+   private final boolean cons(int i)
+   {  switch (b[i])
+      {  case 'a': case 'e': case 'i': case 'o': case 'u': return false;
+         case 'y': return (i==0) ? true : !cons(i-1);
+         default: return true;
+      }
+   }
+
+
+   private final int m()
+   {  int n = 0;
+      int i = 0;
+      while(true)
+      {  if (i > j) return n;
+         if (! cons(i)) break; i++;
+      }
+      i++;
+      while(true)
+      {  while(true)
+         {  if (i > j) return n;
+               if (cons(i)) break;
+               i++;
+         }
+         i++;
+         n++;
+         while(true)
+         {  if (i > j) return n;
+            if (! cons(i)) break;
+            i++;
+         }
+         i++;
+       }
+   }
+
+
+   private final boolean vowelinstem()
+   {  int i; for (i = 0; i <= j; i++) if (! cons(i)) return true;
+      return false;
+   }
+
+   private final boolean doublec(int j)
+   {  if (j < 1) return false;
+      if (b[j] != b[j-1]) return false;
+      return cons(j);
+   }
+
+   private final boolean cvc(int i)
+   {  if (i < 2 || !cons(i) || cons(i-1) || !cons(i-2)) return false;
+      {  int ch = b[i];
+         if (ch == 'w' || ch == 'x' || ch == 'y') return false;
+      }
+      return true;
+   }
+
+   private final boolean ends(String s)
+   {  int l = s.length();
+      int o = k-l+1;
+      if (o < 0) return false;
+      for (int i = 0; i < l; i++) if (b[o+i] != s.charAt(i)) return false;
+      j = k-l;
+      return true;
+   }
+
+
+   private final void setto(String s)
+   {  int l = s.length();
+      int o = j+1;
+      for (int i = 0; i < l; i++) b[o+i] = s.charAt(i);
+      k = j+l;
+   }
+
+   private final void r(String s) { if (m() > 0) setto(s); }
+
+   private final void step()
+   {  if (b[k] == 's')
+      {  if (ends("sses")) k -= 2; else
+         if (ends("ies")) setto("i"); else
+         if (b[k-1] != 's') k--;
+      }
+      if (ends("eed")) { if (m() > 0) k--; } else
+      if ((ends("ed") || ends("ing")) && vowelinstem())
+      {  k = j;
+         if (ends("at")) setto("ate"); else
+         if (ends("bl")) setto("ble"); else
+         if (ends("iz")) setto("ize"); else
+         if (doublec(k))
+         {  k--;
+            {  int ch = b[k];
+               if (ch == 'l' || ch == 's' || ch == 'z') k++;
+            }
+         }
+         else if (m() == 1 && cvc(k)) setto("e");
+     }
+   }
+
+
+   public void stem()
+   {  k = i - 1;
+      if (k > 1) { step(); }
+      i_end = k+1; i = 0;
+   }
+
+}
+
+  public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+    @Override
+    public void reduce(Text word, Iterable<IntWritable> counts, Context context)
+        throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable count : counts) {
+        sum += count.get();
+      }
+      context.write(word, new IntWritable(sum));
+    }
+  }
+}
+
